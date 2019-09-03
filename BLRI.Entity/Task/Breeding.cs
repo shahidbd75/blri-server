9e40d4adf5bfb375e0c27a4cd55f4b30bd876b8e
shahidbd <shahidab@live.com>
2019-09-03 23:21:53 +0600

    change breeding model

--- BLRI.Entity/Task/Breeding.cs
+++ BLRI.Entity/Task/Breeding.cs
@@ -7,10 +7,10 @@ namespace BLRI.Entity.Task
     public class Breeding: BaseEntity
     {
         public Guid AnimalId { get; set; }
-        public DateTime WeaningDate { get; set; }
-        public DateTime FirstHeatDate { get; set; }
-        public DateTime FirstConceptionDate{ get; set; }
-        public DateTime FirstCalvingDate { get; set; }
+        public DateTime? WeaningDate { get; set; }
+        public DateTime? FirstHeatDate { get; set; }
+        public DateTime? FirstConceptionDate{ get; set; }
+        public DateTime? FirstCalvingDate { get; set; }
         public Animal Animal { get; set; }
     }
 }
--- BLRI.ViewModel/Breeding/BreedingViewModel.cs
+++ BLRI.ViewModel/Breeding/BreedingViewModel.cs
@@ -6,9 +6,9 @@ namespace BLRI.ViewModel.Breeding
     public class BreedingViewModel: BaseViewModel<Guid>
     {
         public Guid AnimalId { get; set; }
-        public DateTime WeaningDate { get; set; }
-        public DateTime FirstHeatDate { get; set; }
-        public DateTime FirstConceptionDate { get; set; }
-        public DateTime FirstCalvingDate { get; set; }
+        public DateTime? WeaningDate { get; set; }
+        public DateTime? FirstHeatDate { get; set; }
+        public DateTime? FirstConceptionDate { get; set; }
+        public DateTime? FirstCalvingDate { get; set; }
     }
 }
